aabcaf90d42bc3e05b08efc1c3a455b0c4f7d461
DZaytsev4 <deniszaytsev79524555@gmail.com>
2025-06-07 16:52:31 +0300

    final

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,15 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
+import { useSelector } from '../../services/store/store';
+import { useParams } from 'react-router-dom';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { TIngredient } from '@utils-types';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams<{ id: string }>();
+  const ingredients = useSelector((state) => state.ingredients.data);
+  const ingredientData =
+    ingredients.find((ingredient) => ingredient._id === id) || null;
 
   if (!ingredientData) {
     return <Preloader />;
